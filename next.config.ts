f88108867df554cbbc794fffbd662c499e6792af
Tobias Waslowski <contact@twaslowski.com>
2025-07-22 16:29:03 +0200

    feat(maps): switch maps library for simplicity
    example taken from: https://github.com/lmac-1/google-map-search-nextjs/tree/main

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   env: {
-    NEXT_PUBLIC_MAPS_API_KEY: process.env.GOOGLE_MAPS_API_KEY,
+    NEXT_PUBLIC_GOOGLE_MAPS_API_KEY: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY,
   },
 };
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,32 +1,89 @@
 "use client";
+import { GoogleMap, MarkerF, useLoadScript } from "@react-google-maps/api";
+import { useMemo, useState } from "react";
+import { PlacesAutocomplete } from "@/components/places-autocomplete";
+import { getGeocode, getLatLng } from "use-places-autocomplete";
 
-import { useState } from "react";
-import { APIProvider, Map } from "@vis.gl/react-google-maps";
-import { AutocompleteWebComponent } from "@/components/autocomplete-webcomponent";
-import AutocompleteResult from "@/components/autocomplete-result";
-import Place = google.maps.places.Place;
+// hamburg
+const DEFAULT_LOCATION = { lat: 53.5511, lng: 9.9937 };
 
 export default function Home() {
-  const [selectedPlace, setSelectedPlace] =  useState<Place | null>(null);
+  const [lat, setLat] = useState(DEFAULT_LOCATION.lat);
+  const [lng, setLng] = useState(DEFAULT_LOCATION.lng);
+
+  const libraries = useMemo(() => ["places"], []);
+  // This gives coordinates for the center of the map
+  const mapCenter = useMemo(() => ({ lat, lng }), [lat, lng]);
+
+  const mapOptions = useMemo<google.maps.MapOptions>(
+    () => ({
+      disableDefaultUI: true,
+      clickableIcons: true,
+      scrollwheel: false,
+    }),
+    [],
+  );
+
+  const { isLoaded } = useLoadScript({
+    // your API key will be publicly exposed, so be sure to apply HTTP restrictions on the Google Cloud console
+    googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY as string,
+    // This specifies the google maps libraries that we want to load (e.g. we could add drawing, geometry, places etc)
+    libraries: libraries as any,
+  });
+
+  if (!isLoaded) {
+    return <p>Loading...</p>;
+  }
 
   return (
-    <main>
-      <APIProvider
-        apiKey={"api-key"}
-        version={"beta"}
-      >
-        <Map
-          mapId={"49ae42fed52588c3"}
-          defaultZoom={3}
-          defaultCenter={{ lat: 22.54992, lng: 0 }}
-          gestureHandling={"greedy"}
-          disableDefaultUI={true}
-        >
-          <AutocompleteWebComponent onPlaceSelect={setSelectedPlace} />
+    <main className="flex justify-center align-center">
+      <div className="border border-r-gray-400 w-1/4 h-screen bg-gray-200">
+        <PlacesAutocomplete
+          onAddressSelect={(address) => {
+            getGeocode({ address }).then((results) => {
+              const { lat, lng } = getLatLng(results[0]);
 
-          <AutocompleteResult place={selectedPlace} />
-        </Map>
-      </APIProvider>
+              setLat(lat);
+              setLng(lng);
+            });
+          }}
+        />
+      </div>
+      <GoogleMap
+        options={mapOptions}
+        zoom={12}
+        center={mapCenter}
+        mapTypeId={google.maps.MapTypeId.ROADMAP}
+        mapContainerClassName="w-3/4"
+        // mapContainerStyle={{ width: "800px", height: "800px" }}
+        onLoad={() => console.log("Map component loaded")}
+      >
+        {/* This draws a marker over the map
+          position prop specifies the lat + long of where to place ther marker
+        */}
+        <MarkerF
+          position={mapCenter}
+          onLoad={() => console.log("Marker loaded")}
+          /* you can also change the icon of the marker
+                    icon="https://picsum.photos/64"*/
+        />
+        {/* we can also draw circles around the map */}
+        {/* {[1000, 2500].map((radius, idx) => {
+          return (
+            <CircleF
+              key={idx}
+              center={mapCenter}
+              radius={radius}
+              onLoad={() => console.log("Circle Load...")}
+              options={{
+                fillColor: radius > 1000 ? "red" : "green",
+                strokeColor: radius > 1000 ? "red" : "green",
+                strokeOpacity: 0.8,
+              }}
+            />
+          );
+        })} */}
+      </GoogleMap>
     </main>
   );
 }
